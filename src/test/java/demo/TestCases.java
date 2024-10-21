60946486c03bfbc2c04d1ed4ff92d08e8ab8888e
prashanthkithaneuser <prashanthkithane@gmail.com>
2024-10-21 17:34:32 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -17,6 +19,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
@@ -36,7 +39,7 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
          * Do not change the provided methods unless necessary, they will help in
          * automation and assessment
          */
-        @BeforeTest
+        @BeforeTest(alwaysRun = true)
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
 
@@ -58,7 +61,195 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 driver.manage().window().maximize();
         }
 
-        @AfterTest
+       @Test(priority=1)
+        public void testCase01() throws InterruptedException{
+                try {
+                        // Navigate to YouTube
+                        driver.get("https://www.youtube.com");
+            
+                        // Get the current URL
+                        String currentpageUrl = driver.getCurrentUrl();
+            
+                        // Assert the URL is correct
+                        Assert.assertEquals(currentpageUrl, "https://www.youtube.com/", "Alert! You are not on Youtube page!");
+            
+                        System.out.println("You are on the Youtube page");
+                } catch (Exception e) {
+                        System.out.println("Test Failed: " + e.getMessage());
+                } finally {
+                        // Close the browser
+                        //driver.quit();
+                }
+                //Thread.sleep(5000);
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[text()='About']")); 
+                Thread.sleep(7000);
+                WebElement abouttext=driver.findElement(By.xpath("(//p[@class='lb-font-display-3 lb-font-color-text-primary'])[1]"));
+                System.out.println(abouttext.getText());
+        }
+
+        @Test(priority=2)
+        public void testCase02() throws InterruptedException{
+                try {
+                        // Navigate to YouTube
+                        driver.get("https://www.youtube.com");
+                        Thread.sleep(3000);
+                       
+                } catch (Exception e) {
+                        System.out.println("Test Failed: " + e.getMessage());
+                } 
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='Movies']"));
+                Thread.sleep(3000);
+                Wrappers.clickOnRightScrollOnElementUntillLast(driver, By.xpath("(//button[@aria-label='Next'])[1]"));
+                Thread.sleep(3000); 
+                String certificationtext=Wrappers.extractElementText(driver, By.xpath("(//p[@class='style-scope ytd-badge-supported-renderer'])[last()]"));
+                System.out.println("Movie Certificate Text: "+certificationtext);
+                SoftAssert sassert=new SoftAssert();
+                sassert.assertEquals(certificationtext,"U");
+                String genretextfull=Wrappers.extractElementText(driver, By.xpath("(//span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer'])[last()]"));
+                System.out.println("Full Text: " + genretextfull);
+                // Define the list of categories
+                String[] categories = {"Indian cinema", "Comedy", "Action & adventure", "Animation"};
+
+                // Flag to track if any category matches
+                boolean isCategoryFound = false;
+
+                for (String category : categories) {
+                if (genretextfull.contains(category)) {
+                isCategoryFound = true;  // Mark that we found a matching category
+                System.out.println("Category found: " + category);
+                break;  // Exit the loop since we found a match
+                }
+                }
+
+                // Use soft assertion to check if any category was found
+                if (!isCategoryFound) {
+                 sassert.fail("None of the categories match the genre text: " + genretextfull);
+                }
+                sassert.assertAll();
+                Thread.sleep(2000);
+
+        }
+
+
+       @Test(priority=3)
+        public void testCase03() throws InterruptedException{
+                try {
+                        // Navigate to YouTube
+                        driver.get("https://www.youtube.com");
+                        Thread.sleep(3000);
+                       
+                } catch (Exception e) {
+                        System.out.println("Test Failed: " + e.getMessage());
+                } 
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='Music']"));
+                Thread.sleep(3000);
+                Wrappers.clickOnRightScrollOnElementUntillLast(driver, By.xpath("(//button[@aria-label='Next'])[1]"));
+                Thread.sleep(3000);
+                String noofvideostext=Wrappers.extractElementText(driver, By.xpath("(//p[@id='video-count-text'])[11]"));
+                System.out.println("Text of video count: "+noofvideostext);
+                String intpart=noofvideostext.substring(0,2);
+                System.out.println("Split video count: "+intpart);
+                int videocount=Integer.parseInt(intpart);
+                System.out.println("Video Count: "+videocount);
+                SoftAssert s1=new SoftAssert();
+                s1.assertTrue(videocount<=50,"Video count exceeds 50");
+                s1.assertAll();
+                Thread.sleep(2000);
+        }
+
+        @Test(priority=4)
+        public void testCase04() throws InterruptedException{
+                try {
+                        // Navigate to YouTube
+                        driver.get("https://www.youtube.com");
+                        Thread.sleep(3000);
+                       
+                } catch (Exception e) {
+                        System.out.println("Test Failed: " + e.getMessage());
+                } 
+                Wrappers.clickOnElementWrapper(driver, By.xpath("//a[@title='News']"));
+                Thread.sleep(3000);
+                //WebElement latestnewselem=driver.findElement(By.xpath("(//div[@id='contents'])[18]"));
+                List<WebElement> lnews=driver.findElements(By.xpath("//div[@class='style-scope ytd-post-renderer' and @id='dismissible']"));
+                int count=0;
+                int likescount=0;
+                for(WebElement elem:lnews){
+                        JavascriptExecutor js = (JavascriptExecutor) driver;
+                        js.executeScript("arguments[0].scrollIntoView(true);", elem);
+                        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                        wait.until(ExpectedConditions.elementToBeClickable(elem));
+                        String newstitle=elem.findElement(By.xpath(".//span[@class='style-scope ytd-post-renderer']")).getText();
+                        System.out.println("Trending news title is: "+newstitle);
+                        String newsdescription=elem.findElement(By.xpath(".//span[@dir='auto']")).getText();
+                        //System.out.println("Trending news description is: "+newsdescription);
+                        String likestext=elem.findElement(By.xpath(".//span[@id='vote-count-middle']")).getText();
+                        System.out.println("Likes String: "+likestext);
+                        int likes=Integer.parseInt(likestext.trim());
+                        System.out.println("Likes count of news: "+likes);
+                        likescount+=likes;
+
+                        count++;
+                        if(count>=3){
+                                break;
+                        }
+
+                }
+                System.out.println("Total Likes: "+likescount);
+        }
+
+
+        @Test(priority=5,dataProvider = "excelData", dataProviderClass = ExcelDataProvider.class)
+        public void testCase05(String searchTerm) throws InterruptedException{
+                try {
+                        // Navigate to YouTube
+                        driver.get("https://www.youtube.com");
+                        Thread.sleep(3000);
+                       
+                } catch (Exception e) {
+                        System.out.println("Test Failed: " + e.getMessage());
+                } 
+        WebElement searchBox = driver.findElement(By.name("search_query"));
+        searchBox.sendKeys(searchTerm);
+        searchBox.submit();
+
+        Thread.sleep(2000);  // Wait for the results to load
+
+        // Initialize view count sum
+        long totalViews = 0;
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        while (totalViews < 100_000_000) {  // Stop when total views exceed 10 crore (100 million)
+            // Find all video elements with view count
+            List<WebElement> videoElements = driver.findElements(By.xpath("//span[contains(@class,'style-scope ytd-video-meta-block')]"));
+
+            for (WebElement video : videoElements) {
+                String viewText = video.getText();
+                totalViews += Wrappers.extractViewCount(viewText);
+                if (totalViews >= 100_000_000) {
+                    break;
+                }
+            }
+
+            // Scroll down to load more results
+            js.executeScript("window.scrollBy(0,1000);");
+            Thread.sleep(2000);  // Allow new results to load
+        }
+        SoftAssert  s1=new SoftAssert();
+        // Assert the total views exceeded 10 crore
+        s1.assertTrue(totalViews >= 100_000_000, "Total views for '" + searchTerm + "' did not reach 10 crore!");
+
+        // Log the result
+        System.out.println("Search term: " + searchTerm + ", Total views: " + totalViews);
+
+        // Assert all
+        s1.assertAll();
+
+
+        }
+
+
+
+       @AfterTest
         public void endTest() {
                 driver.close();
                 driver.quit();
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,17 +1,149 @@
 package demo.wrappers;
 
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+     public static void clickOnElementWrapper(WebDriver driver, By locator) {
+        System.out.println("Clicking on Element");
+        Boolean success;
+        try {
+            // Locate the element first
+            WebElement element = driver.findElement(locator);
+    
+            // Use JavaScript to scroll the element into view
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView(true);", element);
+            System.out.println("Scrolled to the element successfully!");
+    
+            // Wait until the element is visible
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    
+            // Click the element
+            Thread.sleep(3000); // Optional delay before clicking
+            element.click();
+            Thread.sleep(3000); // Optional delay after clicking
+            System.out.println("Successfully clicked on element.");
+            success = true;
+    
+        } catch (Exception e) {
+            System.out.println("Exception Occurred! " + e.getMessage());
+            success = false;
+        }
+    }
+    
+
+    public static void clickOnRightScrollOnElementUntillLast(WebDriver driver, By locator) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement rightScrollButton = driver.findElement(locator);
+        System.out.println("Trying to scroll to the right:");
+    
+        while (true) {
+            try {
+                // Scroll into view to make sure the button is visible
+                js.executeScript("arguments[0].scrollIntoView(true);", rightScrollButton);
+                
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                wait.until(ExpectedConditions.elementToBeClickable(rightScrollButton));
+
+                // Check if the button is enabled and clickable before clicking
+                if (rightScrollButton.isEnabled() && rightScrollButton.isDisplayed()) {
+                    // Click the right scroll button
+                    rightScrollButton.click();
+                    System.out.println("Clicked on the scroller.");
+    
+                    // Wait a little before the next click to avoid issues
+                    Thread.sleep(1000);
+                } else {
+                    System.out.println("Reached the end of the scrollable area (button disabled or hidden).");
+                    break; // Exit the loop if the button is no longer clickable
+                }
+    
+                // Re-locate the button in case the DOM changes after scrolling
+                rightScrollButton = driver.findElement(By.xpath("(//button[@aria-label='Next'])[1]"));
+    
+            } catch (org.openqa.selenium.ElementClickInterceptedException e) {
+                // Handle click interception, retry scrolling or adjust the interaction
+                System.out.println("ElementClickInterceptedException occurred: " + e.getMessage());
+                js.executeScript("window.scrollBy(0,0);"); // Slightly scroll the window to avoid interception
+            } catch (org.openqa.selenium.StaleElementReferenceException e) {
+                // Handle stale element, re-locate the button and continue
+                System.out.println("StaleElementReferenceException occurred. Re-locating the button...");
+                rightScrollButton = driver.findElement(By.xpath("(//button[@aria-label='Next'])[1]"));
+            } catch (org.openqa.selenium.NoSuchElementException e) {
+                // Exit if the button is no longer found (end of scrollable area)
+                System.out.println("NoSuchElementException: Scroll completed, no further interaction possible.");
+                break;
+            } catch (Exception e) {
+                // Catch any other unexpected exceptions
+                System.out.println("Unexpected exception occurred: " + e.getMessage());
+                break;
+            }
+        }
+    }
+    
+    
+
+    public static String extractElementText(WebDriver driver, By locator) {
+        String elementText = "";
+        try {
+            // Locate the element using the provided locator
+            WebElement element = driver.findElement(locator);
+            Thread.sleep(2000);
+            // Extract the text from the element
+            elementText = element.getText().trim();
+        } catch (Exception e) {
+            System.out.println("An error occurred while getting the element's text: " + e.getMessage());
+        }
+        return elementText;
+    }
+
+    
+
+     // Helper method to extract view count from string
+     public static  long extractViewCount(String viewText) {
+        long views = 0;
+        try {
+            // Example: "1.2M views", "534K views", "10K views"
+            if (viewText.contains("M")) {
+                views = (long) (Double.parseDouble(viewText.replace("M views", "")) * 1_000_000);
+            } else if (viewText.contains("K")) {
+                views = (long) (Double.parseDouble(viewText.replace("K views", "")) * 1_000);
+            } else if (viewText.contains("views")) {
+                views = Long.parseLong(viewText.replace(" views", "").replace(",", ""));
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to parse view count: " + e.getMessage());
+        }
+      
+        return views;
+    }
+
+
+
 }
